b74c0c7dae77e1dbab8341b785ee7b0e99a9b8ca
Caio Corsini <caiofilho14@gmail.com>
2024-10-25 16:32:13 -0300

    Mais comentários importantes

--- tempoEvento.py
+++ tempoEvento.py
@@ -41,13 +41,20 @@ quant_seg_total = quant_seg_fim - quant_seg_ini  # Diferença em segundos de fim
 
 quantidades = [0,0,0,0]  # Vetor auxiliar para armazenar quantidades de dias, horas, minutos e segundos respectivamente
 valores = [QUANT_SEGUNDOS_DIA, QUANT_SEGUNDOS_HORA, QUANT_SEGUNDOS_MINUTO, 1]
-i = 0  # Variável contadora auxiliar
+i = 0  # Variável contadora auxiliar para determinar o que usar: dia, hora, minuto ou segundo (vetor logo acima)
 
-while quant_seg_total > 0:
+# Vou diminuindo a quant de segundos com o máximo de dias que puder, depois horas, depois minutos e depois segundos
+while quant_seg_total > 0:  # Vai diminuindo até não sobrarem mais segundos
     if valores[i] <= quant_seg_total:
         quant_seg_total -= valores[i]
         quantidades[i] += 1
     else:
-        i += 1
+        i += 1 # Passo a usar outro valor para diminuir a quantidade de segundos restante
 
+# Impressão formatada do resultado
 print(f"{quantidades[0]} dia(s)\n{quantidades[1]} hora(s)\n{quantidades[2]} minuto(s)\n{quantidades[3]} segundo(s)")
+
+'''
+PONTOS IMPORTANTES
+1. Quem tiver curiosidade, observar algoritmo guloso do "problema do troco", o qual usa uma estratégia similar
+'''
