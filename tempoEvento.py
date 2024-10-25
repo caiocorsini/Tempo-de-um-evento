0cad6bd47d76474659bdd85895c457cbe6d1ed74
Caio Corsini <caiofilho14@gmail.com>
2024-10-25 16:14:42 -0300

    Comentários adicionados

--- tempoEvento.py
+++ tempoEvento.py
@@ -1,36 +1,47 @@
+# Constantes de quantidades de segundos
 QUANT_SEGUNDOS_DIA = 86400
 QUANT_SEGUNDOS_HORA = 3600
 QUANT_SEGUNDOS_MINUTO = 60
 
+#def total_segundos(dias, horas, minutos, segundos)
 
+# Todos os inputs que o usuário vai dar
 dia_ini_string = input()
 horario_ini_string = input()
 dia_fim_string = input()
 horario_fim_string = input()
 
-split_aux = dia_ini_string.split(" ")
+# PROCESSAMENTO DE INPUTS: Como todos os inputs vem na forma de strings, preciso extrair os inteiros delas.
+# Uso função split(<token>) a qual converte uma string em uma lista usando um <token> como separador
+# Processando dias
+split_aux = dia_ini_string.split(" ") # "Dia 5" -> ["Dia", "5"]
 dia_ini = int(split_aux[1])
 split_aux = dia_fim_string.split(" ")
 dia_fim = int(split_aux[1])
 
-split_aux = horario_ini_string.split(" ")
-hora_ini = int(split_aux[0])
+# Processando horários
+# Horário de início
+split_aux = horario_ini_string.split(" ") # 08 : 30 : 40 -> ["08", ":", "30", ":", "40"]
+hora_ini = int(split_aux[0])  # Ainda estão como strings, precisa converter para int para fazer os cálculos
 minuto_ini = int(split_aux[2])
 segundo_ini = int(split_aux[4])
 
+# Horário de fim
 split_aux = horario_fim_string.split(" ")
 hora_fim = int(split_aux[0])
 minuto_fim = int(split_aux[2])
 segundo_fim = int(split_aux[4])
 
-quant_seg_ini = QUANT_SEGUNDOS_DIA * dia_ini + QUANT_SEGUNDOS_HORA * hora_ini + QUANT_SEGUNDOS_MINUTO * minuto_ini + segundo_ini
-quant_seg_fim = QUANT_SEGUNDOS_DIA * dia_fim + QUANT_SEGUNDOS_HORA * hora_fim + QUANT_SEGUNDOS_MINUTO * minuto_fim + segundo_fim
-quant_seg_total = quant_seg_fim - quant_seg_ini
-
-quant_dias = 0
-quant_horas = 0
-quant_minutos = 0
-quant_segundos = 0
+# Somando as quantidades de segundos no total para início e para fim
+quant_seg_ini = (QUANT_SEGUNDOS_DIA * dia_ini +
+                 QUANT_SEGUNDOS_HORA * hora_ini +
+                 QUANT_SEGUNDOS_MINUTO * minuto_ini +
+                 segundo_ini)
+quant_seg_fim = (QUANT_SEGUNDOS_DIA * dia_fim +
+                 QUANT_SEGUNDOS_HORA * hora_fim +
+                 QUANT_SEGUNDOS_MINUTO * minuto_fim +
+                 segundo_fim)
+quant_seg_total = quant_seg_fim - quant_seg_ini  # Diferença em segundos de fim e início
 
 quantidades = [0,0,0,0]
 valores = [QUANT_SEGUNDOS_DIA, QUANT_SEGUNDOS_HORA, QUANT_SEGUNDOS_MINUTO, 1]
