54bb64bfb158d5274df6560965cb8be6a08e16f2
Caio Corsini <caiofilho14@gmail.com>
2024-10-25 16:26:07 -0300

    Agora usa função map para mapear segundos

--- tempoEvento.py
+++ tempoEvento.py
@@ -1,4 +1,5 @@
 # Constantes de quantidades de segundos
+# Escolhi converter tudo especificamente para segundos para não ter que trabalhar com floats
 QUANT_SEGUNDOS_DIA = 86400
 QUANT_SEGUNDOS_HORA = 3600
 QUANT_SEGUNDOS_MINUTO = 60
@@ -26,29 +27,23 @@ dia_fim = int(split_aux[1])
 
 # Processando horários
 # Ainda estão como strings, precisa converter para int para fazer os cálculos
+# EXEMPLO CONVERSÃO - 08 : 30 : 40 -> ["08", "30", "40"]
 # Horário de início
-split_aux = horario_ini_string.split(" : ") # 08 : 30 : 40 -> ["08", "30", "40"]
-hora_ini = int(split_aux[0])
-minuto_ini = int(split_aux[1])
-segundo_ini = int(split_aux[2])
+hora_ini, minuto_ini, segundo_ini = map(int, horario_ini_string.split(" : "))
 
 # Horário de fim
-split_aux = horario_fim_string.split(" : ")
-hora_fim = int(split_aux[0])
-minuto_fim = int(split_aux[1])
-segundo_fim = int(split_aux[2])
+hora_fim, minuto_fim, segundo_fim = map(int, horario_fim_string.split(" : "))
 
 # Somando as quantidades de segundos no total para início e para fim
 quant_seg_ini = total_segundos(dia_ini, hora_ini, minuto_ini, segundo_ini)
 quant_seg_fim = total_segundos(dia_fim, hora_fim, minuto_fim, segundo_fim)
 quant_seg_total = quant_seg_fim - quant_seg_ini  # Diferença em segundos de fim e início
 
-quantidades = [0,0,0,0]
+quantidades = [0,0,0,0]  # Vetor auxiliar para armazenar quantidades de dias, horas, minutos e segundos respectivamente
 valores = [QUANT_SEGUNDOS_DIA, QUANT_SEGUNDOS_HORA, QUANT_SEGUNDOS_MINUTO, 1]
-i = 0
+i = 0  # Variável contadora auxiliar
 
 while quant_seg_total > 0:
-    #print(quant_seg_total)
     if valores[i] <= quant_seg_total:
         quant_seg_total -= valores[i]
         quantidades[i] += 1
